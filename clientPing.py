1eda38b7e66e7857b109c089d494ba5a03a40abb
Emmama222 <20544166@student.westernsydney.edu.au>
2024-01-09 20:07:22 +1100

    Changed format of Robotid so it works with code

--- clientPing.py
+++ clientPing.py
@@ -2,11 +2,11 @@ import socket
 
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-Robot_id = 1
+Robot_id = b'1'
 IP = "127.0.0.1"
 PORT = 2024
 
-msg = bytes(Robot_id)
+msg = Robot_id
 clientSock.sendto(msg,(IP, PORT))
 
 data,addr = clientSock.recvfrom(1024)

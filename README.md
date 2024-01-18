ece1940e0864d03adfebb24fbed94aa4e78f8ed6
Oliver Obst <o.obst@westernsydney.edu.au>
2024-01-18 23:04:58 +1100

    initial quali24 file structure

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# code
-Software, including firmware, control algorithms, etc.
+# Code
+
+This repo contains the software for our team. This includes the
+overall team control, and also the code that translates actions into
+motor commands, our motor control.
+
+## TeamControl
+
+TeamControl is a centralised software intended to run on a machine
+next to the field, and communicate with all other SSL systems and our
+robots.
+
+
+## MotorControl
+
+This module is intended to be run on a raspi, on each robot, to
+receive and translate actions into motor commands.
---
+++ TeamControl/teamcontrol.py
@@ -0,0 +1,37 @@
+import threading
+import time
+
+class TeamControl:
+    def __init__(self, world_model, skills):
+        self.world_model = world_model
+        self.skills = skills  # A collection of skills
+        self.current_skill = None
+
+    def select_skill(self):
+        # Logic to select the appropriate skill based on the world model
+        pass
+
+    def run_skill_loop(self):
+        while True:
+            print("skill loop")
+            time.sleep(5)  # Skill execution rate
+
+    def start(self):
+        # Start the WorldModel update loop in a separate thread
+        threading.Thread(target=self.world_model.update_loop).start()
+
+        # Start the skill execution loop
+        self.run_skill_loop()
+
+class WorldModel:
+    def update_loop(self):
+        while True:
+            print("WorldModel update")
+            time.sleep(3)
+
+# Example usage:
+world_model = WorldModel()
+skills = None
+tc = TeamControl(world_model, skills)
+tc.start()
+

1d81a219ce80066c1202686267502432a99c29d0
Emmama222 <20544166@student.westernsydney.edu.au>
2024-01-20 20:48:55 +1100

    converted codes into module - TeamControl

---
+++ src/TeamControl/receiver.py
@@ -0,0 +1,23 @@
+import sslclient
+from worldmodel import WorldModel as wm
+
+ssl_vision_ip = "224.5.23.2" #ip connected to ssl-vision
+c = sslclient.client(ssl_vision_ip, port=10006) # connect to ssl-vision
+c.connect()
+
+while wm.state == "UPDATE":
+    #received decoded pyackage
+    data = c.receive()
+    
+    # if we wanted to get the data about the field (whitelines)
+    if data.HasField('geometry'):
+        #print("geometry : ",data.geometry)
+        wm.update_geometry(data.geometry)
+    
+    # if we want to know about the robots and ball data
+    if data.HasField('detection'):
+        #print("detection : ", data.detection) #debug
+        # updates the world model with received information
+        wm.update_detection(data.detection)
+        # example if you want to get the robot position
+        #print(w.get_robot_position(0,True))

c4439f7803fe15419b67b48620bff46d0752b5e8
Jason N <7614175+jsnng@users.noreply.github.com>
2024-01-21 23:29:32 +1100

    update world model function added

--- src/WSUSSL/World/receiver.py
+++ src/WSUSSL/World/receiver.py
@@ -1,23 +1,14 @@
 import sslclient
-from WSUSSL.World.model import Model as wm
 
-ssl_vision_ip = "224.5.23.2" #ip connected to ssl-vision
-c = sslclient.client(ssl_vision_ip, port=10006) # connect to ssl-vision
-c.connect()
+def update_world_model(model):
+    ssl_vision_ip_addr = "244.5.23.2"
+    client = sslclient.client(ssl_vision_ip_addr, port=10006)
+    client.connect()
 
-while wm.state == "UPDATE":
-    #received decoded pyackage
-    data = c.receive()
-    
-    # if we wanted to get the data about the field (whitelines)
+    data = client.receive()
+
     if data.HasField('geometry'):
-        #print("geometry : ",data.geometry)
-        wm.update_geometry(data.geometry)
-    
-    # if we want to know about the robots and ball data
+        model.update_geometry(data.geometry)
+
     if data.HasField('detection'):
-        #print("detection : ", data.detection) #debug
-        # updates the world model with received information
-        wm.update_detection(data.detection)
-        # example if you want to get the robot position
-        #print(w.get_robot_position(0,True))
+        model.update_detection(data.detection)

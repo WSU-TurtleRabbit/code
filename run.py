69f31865460e514860e94115ff61dfac17cc0aa6
Emmama222 <20544166@studnet.westernsydney.edu.au>
2024-01-27 17:34:28 +1000

    test1

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
+from WSUSSL.Networking.ServerClass import Server
+from WSUSSL.Shared.utils import main as interface
 
 if __name__ ==  '__main__':
     # call world.model wm
     # connect to other script (sending wm)
-    pass
+
+    server = Server()
+    server.send_message(interface())

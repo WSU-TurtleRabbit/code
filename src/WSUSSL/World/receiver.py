b7c17887cb09072ef547c3bcc43cdd3be2dc6cea
Emmama222 <>
2024-02-06 23:08:30 +1100

    refactor world model receiver

--- src/WSUSSL/World/receiver.py
+++ src/WSUSSL/World/receiver.py
@@ -1,51 +1,58 @@
 from WSUSSL.World.proto2 import ssl_vision_wrapper_pb2
 from WSUSSL.World.proto2 import grSim_Packet_pb2
 from WSUSSL.World.proto2 import grSim_Commands_pb2
+from WSUSSL.World.model import Model
 
 import socket
 
-class proto2_ssl_vision_py_receiver():
-    def __init__(self, ssl_vision_ip_addr="224.5.23.2", ssl_vision_port=10006):
-        self.ssl_vision_ip_addr = ssl_vision_ip_addr
-        self.ssl_vision_port = ssl_vision_port
-        self.ssl_vision_client = None
-
-    def connect(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.client.bind((self.ssl_vision_ip_addr, self.ssl_vision_port))
-
-    def update_world_model(self, model):
-        if self.client is None:
-            raise UserWarning('call connect() before update_world_model()')
-        
-        while True:
-            data, addr = self.client.recvfrom(1024)
-            ssl_vision_wrapper = ssl_vision_wrapper_pb2.SSL_WrapperPacket()
-            wrapper = ssl_vision_wrapper.FromString(data)
-
-            if wrapper.HasField('detection'):
-                model.update_detection(wrapper.detection)
-
-            if wrapper.HasField('geometry'):
-                model.update_detection(data.geometry)
-
-class proto2_grsim_py_receiver():
-    def __init__(self, grsim_ip_addr, grsim_port):
-        self.grsim_ip_addr = grsim_ip_addr
-        self.grsim_port = grsim_port
+class proto2_ssl_receiver:
+    def __init__(self, ip_addr, port):
+        self.ip_addr = ip_addr
+        self.port = port
         self.client = None
 
     def connect(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.client.bind((self.grsim_ip_addr, self.grsim_port))
+        self.client.bind((self.ip_addr, self.port))
 
-    def update_world_model(self, model):
+    def listen(self):
         if self.client is None:
-            raise UserWarning('call connect() before update_world_model()')
+            raise UserWarning('connect() needs to be called before listen()')
+        
+        if not isinstance(self.model, Model):
+            raise TypeError(f'expected Model, got {self.model.__class__}')
         
         while True:
             data, addr = self.client.recvfrom(1024)
-            
+            self.update_world_model(data)
+    
+    def update_world_model(self, data):
+        raise NotImplementedError
+
+    def set_world_model(self, model):
+        self.model = model
+
+class proto2_ssl_vision_py_receiver(proto2_ssl_receiver):
+    def __init__(self, ip_addr="224.5.23.2", port=10006):
+        super().__init__(ip_addr, port)
+  
+    def update_world_model(self, data):
+        ssl_vision_wrapper = ssl_vision_wrapper_pb2.SSL_WrapperPacket()
+        wrapper = ssl_vision_wrapper.FromString(data)
+
+        if wrapper.HasField('detection'):
+            self.model.update_detection(wrapper.detection)
+
+        if wrapper.HasField('geometry'):
+            self.model.update_detection(data.geometry)
+
+class proto2_grsim_py_receiver():
+    def __init__(self, ip_addr, port):
+        super().__init__(ip_addr, port)
+
+    def update_world_model(self, model):
+        pass
+
 class proto2_grsim_py_generator():
     def __init__(self, grsim_ip_addr, grsim_port):
         self.grsim_ip_addr = grsim_ip_addr

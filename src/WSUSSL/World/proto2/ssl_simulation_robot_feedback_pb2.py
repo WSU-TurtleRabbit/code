92f877d7d20cc287893ea1eaaa2f338064d35de8
Emmama222 <>
2024-02-06 21:01:12 +1100

    fix import errors

--- src/WSUSSL/World/proto2/ssl_simulation_robot_feedback_pb2.py
+++ src/WSUSSL/World/proto2/ssl_simulation_robot_feedback_pb2.py
@@ -12,7 +12,7 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
-import ssl_simulation_error_pb2 as ssl__simulation__error__pb2
+import WSUSSL.World.proto2.ssl_simulation_error_pb2 as ssl__simulation__error__pb2
 from google.protobuf import any_pb2 as google_dot_protobuf_dot_any__pb2
 
 
--- src/WSUSSL/World/receiver.py
+++ src/WSUSSL/World/receiver.py
@@ -28,6 +28,48 @@ class proto2_ssl_vision_py_receiver():
 
             if wrapper.HasField('geometry'):
                 model.update_detection(data.geometry)
+
+class proto2_grsim_py_receiver():
+    def __init__(self, grsim_ip_addr, grsim_port):
+        self.grsim_ip_addr = grsim_ip_addr
+        self.grsim_port = grsim_port
+        self.client = None
+
+    def connect(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client.bind((self.grsim_ip_addr, self.grsim_port))
+
+    def update_world_model(self, model):
+        if self.client is None:
+            raise UserWarning('call connect() before update_world_model()')
+        
+        while True:
+            data, addr = self.client.recvfrom(1024)
+            
+class proto2_grsim_py_generator():
+    def __init__(self, grsim_ip_addr, grsim_port):
+        self.grsim_ip_addr = grsim_ip_addr
+        self.grsim_port = grsim_port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    def send(self, message):
+        self.socket.sendto(message, (self.grsim_ip_addr, self.grsim_port))
+
+    @staticmethod
+    def grsim_robot_command(id, kickspeedx, kickspeedz, veltangent, velnormal,
+                            velangular, spinner, wheelspeed, wheel1=0.0, wheel2=0.0, 
+                            wheel3=0.0, wheel4=0.0):
+        
+        return grSim_Commands_pb2.grSim_Robot_Command(
+            id, kickspeedx, kickspeedz, veltangent, velnormal, velangular, 
+            spinner, wheelspeed, wheel1, wheel2, wheel3, wheel4
+        )
+    
+    @staticmethod
+    def grim_commands(timestamp, isteamyellow, robot_commands):
+        return grSim_Commands_pb2.grSim_Commands(
+            timestamp, isteamyellow, robot_commands
+        )
     
 if __name__ == '__main__':
     recv = proto2_ssl_vision_py_receiver('127.0.0.1', 50514)

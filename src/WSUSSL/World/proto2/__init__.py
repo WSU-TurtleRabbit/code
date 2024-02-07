6a723a80676a5c89e5fde7225cc1608e375e2316
Emmama222 <20544166@student.westernsydney.edu.au>
2024-02-07 12:07:05 +1100

    modify the init script

--- src/WSUSSL/World/proto2/__init__.py
+++ src/WSUSSL/World/proto2/__init__.py
@@ -1,1 +1,1 @@
-__init__.py.old s/import WSUSSL.World.proto2/import WSUSSL.World.proto2WSUSSL.World.proto2/g
+#__init__.py.old s/import WSUSSL.World.proto2/import WSUSSL.World.proto2WSUSSL.World.proto2/g
